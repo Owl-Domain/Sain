e896683e9c80a32c5d95d6fefba8f136470518de
Nightowl <nightowl286@protonmail.com>
2025-01-26 18:20:47 +0000

    + Added the initial Sain and Sain.Abstractions projects.

---
+++ src/Sain/Sain/global/global.Common.cs
@@ -0,0 +1,10 @@
+global using System;
+global using System.Collections;
+global using System.Collections.Generic;
+global using System.ComponentModel;
+global using System.Diagnostics;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Runtime.CompilerServices;
+global using System.Runtime.Versioning;
+global using System.Threading;
+global using System.Threading.Tasks;
